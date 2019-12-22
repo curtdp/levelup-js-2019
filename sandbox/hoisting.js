704505de5d75ea83bebb79157f4022a42ed2e5ad
Igor Oliinyk <igor.oleynikdp@gmail.com>
2019-12-22 21:44:40 +0200

    Initial commit

---
+++ sandbox/hoisting.js
@@ -0,0 +1,15 @@
+var test = 'hello';
+
+const str = `  Hello ${test}`;
+
+console.log(str);
+
+const a = 'stra';
+
+let age = 100;
+
+let el = document.createElement('p');
+
+document.querySelector('p').appendChild(el);
+
+let b = 5;
