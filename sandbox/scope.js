139c10fa2127db1d99db1b01627f66bd7fa5347f
Igor Oliinyk <igor.oleynikdp@gmail.com>
2020-01-13 18:57:25 +0200

    Scope & Closures

---
+++ sandbox/scope.js
@@ -0,0 +1,61 @@
+/* eslint-disable */
+
+// window object
+const first = 'Igor';
+let second = 'Oliinyk';
+var age = 100;
+
+// function sayHi() {
+//   console.log('Hi!');
+// }
+
+// Scope Lookup
+// const age = 100;
+
+// function go() {
+//   const myAge = 50;
+//   const hair = 'blonde';
+//   console.log(myAge);
+//   console.log(age);
+//   console.log(hair);
+// }
+
+// go();
+
+// block scope
+
+// function isCool(name) {
+//   let cool;
+//   if (name === 'Igor') {
+//     cool = true;
+//   }
+
+//   console.log(cool);
+//   return cool;
+// }
+// isCool('Igor');
+
+// var leaking
+// for (var i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// const dog = 'Tobik';
+
+// function logDog(dog) {
+//   console.log(dog);
+// }
+
+// function go() {
+//   const dog = 'Barney';
+//   logDog('Rufus');
+// }
+
+// go();
+
+// function sayHi(name) {
+//   function yell() {
+//     console.log(name.toUpperCase());
+//   }
+//   yell();
+// }
