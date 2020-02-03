2ba1bbe1e4b48f867daeaddb5e628792ec0d9240
Igor Oliinyk <igor.oleynikdp@gmail.com>
2020-02-03 20:57:49 +0200

    черепаха

--- sandbox/turtle.js
+++ sandbox/turtle.js
@@ -1,1 +1,50 @@
-console.log('it works');
+const turt = document.querySelector('.turt');
+
+console.log(turt.style.left);
+let x = 0;
+let y = 0;
+let isBack = false;
+let rotate = 0;
+
+function handleKey(e) {
+  if (!e.key.includes('Arrow')) return;
+
+  switch (e.key) {
+    case 'ArrowUp':
+      y = y - 1;
+      console.log('Up');
+      rotate = -90;
+      break;
+    case 'ArrowRight':
+      console.log('Right');
+      isBack = false;
+      x = x + 1;
+      rotate = 0;
+      break;
+    case 'ArrowDown':
+      y = y + 1;
+      console.log('Down');
+      rotate = 90;
+      break;
+    case 'ArrowLeft':
+      console.log('Left');
+      isBack = true;
+      x = x - 1;
+      rotate = 0;
+      break;
+    default:
+      console.log('This was not arrow key');
+  }
+
+  const speed = 10;
+  turt.style.setProperty('--x', `${x * speed}px`);
+  turt.style.setProperty('--y', `${y * speed}px`);
+  turt.style.setProperty('--rotate', `${rotate}deg`);
+  if (isBack) {
+    turt.style.setProperty('--flip', '180deg');
+  } else {
+    turt.style.setProperty('--flip', '0');
+  }
+}
+
+window.addEventListener('keydown', handleKey);
