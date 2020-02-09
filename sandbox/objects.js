1622c9df1b0c13da221ecc84fc63ca667f027ca4
Igor Oliinyk <curtdp@gmail.com>
2020-02-09 13:02:43 +0200

    Новый код

--- sandbox/objects.js
+++ sandbox/objects.js
@@ -1,5 +1,9 @@
 /*eslint comma-dangle: ["error", "always-multiline"]*/
 
+function sayHello() {
+  console.log(`Hello ${this.last}`);
+}
+
 const person = {
   first: 'Igor',
   last: 'Oliinyk',
@@ -11,6 +15,14 @@ const person = {
   },
   777: '777 Value',
   newKey: 'Something new',
+  newKey2: 'Something new2',
+  sayHi() {
+    console.log(`Hi!!! ${this.first}`);
+  },
+  sayHello,
+  sayMeow: () => {
+    console.log(`Meowww!! ${this}`);
+  },
 };
 
 const key = 'first';
@@ -25,3 +37,66 @@ console.log(person[777]);
 const person2 = new Object(42);
 
 console.log(person2);
+
+const person3 = person;
+console.log(person3.first);
+person3.first = 'Vasya';
+console.log(person3.first);
+console.log(person.first);
+
+const person4 = { ...person };
+console.log(person4.first);
+person4.first = 'Oleg';
+person4.gadgets.laptop = 'Ubuntu';
+console.log(person4.first);
+console.log(person.first);
+console.log(person4.gadgets.laptop);
+console.log(person.gadgets.laptop);
+
+const person5 = _.cloneDeep(person);
+console.log(person5.gadgets.laptop);
+person5.gadgets.laptop = 'CentOS';
+console.log(person5.gadgets.laptop);
+console.log(person.gadgets.laptop);
+
+// person.pets = {
+//   dog: 'Sparky',
+// };
+
+// const dogName = person.pets ? person.pets.dog : `Person has no dog`;
+// console.log(dogName);
+
+if (person.pets && person.pets.dog) {
+  console.log(person.pets.dog);
+}
+
+// console.log(person.pets.dog);
+console.log(delete person.gadgewerts1);
+console.log(person);
+
+Object.defineProperty(person, 'bike', {
+  value: 'XB3',
+  writable: false,
+  configurable: true,
+});
+
+person.bike = 'Giant';
+
+console.log(person.bike);
+// console.log(delete person.bike);
+
+Object.defineProperty(person, 'bike', {
+  writable: true,
+});
+
+person.bike = 'Trek';
+console.log(person.bike);
+
+console.log(delete person.bike);
+
+person.sayHi();
+person.sayHello();
+
+person.sayMeow();
+
+console.log(this);
