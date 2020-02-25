359c16e32f76361152a0bd6d1fea7cb0ee35143b
Igor Oliinyk <curtdp@gmail.com>
2020-02-25 11:03:42 +0200

    new code

---
+++ sandbox/scalpelFind.js
@@ -0,0 +1,12 @@
+import { bigOak } from './crow-tech';
+
+async function locateScalpel(nest) {
+  // Your code here.
+}
+
+function locateScalpel2(nest) {
+  // Your code here.
+}
+
+locateScalpel(bigOak).then(console.log);
+// → Butcher Shop
