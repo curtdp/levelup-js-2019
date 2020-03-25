3fd4ce6c29331ae8e53c5c9e89e88625773397d1
Igor Oliinyk <igor.oleynikdp@gmail.com>
2020-03-25 08:32:19 +0200

    Новый код

---
+++ exercises/25-vue-cli-demo/trivia-app/src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="app" class="container mx-auto">
+    <h1 class="text-4xl">Trivia app</h1>
+    <div>
+      Модальное окно
+    </div>
+    <!-- вопрос trivia -->
+    <div class="max-w-sm mx-auto">
+      <div class="flex items-center justify-between">
+      <div>
+        <label for="category" class="mb-2 block">Select category</label>
+        <select class="border rounded w-4/5" name="category" id="category" v-model="selectedCategory">
+          <option v-for="category in categories" :key="category.id" :value="category.id">{{ category.name }}</option>
+        </select>
+      </div>
+      <!-- следим за результатами игры -->
+      <div class="text-4xl flex mt-4 md:mt-0 justify-end">
+        <div class="px-4 text-right">
+          <span class="text-red-700">{{ countOfWrongAnswers }}</span>
+          <div class="text-red-700 text-sm">wrong</div>
+        </div>
+        <div class="px-4 text-right">
+          <span class="text-green-700">{{ countOfRightAnswers }}</span>
+          <div class="text-green-700 text-sm">correct</div>
+
+          <!-- Вопросы -->
+
+
+
+          <div class="mt-5 text-right">
+          <button
+            @click="getOneQuestion"
+            class="bg-green-200 p-2 text-xs text-green-500 border border-green hover:bg-green-lightest rounded"
+          >Go to next question 👉</button>
+        </div>
+        </div>
+      </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      question: null,
+      isCorrect: null,
+      correctStreak: 0,
+      countOfWrongAnswers: 0,
+      countOfRightAnswers: 0,
+      selectedCategory: "any",
+      categories: []
+    }
+  },
+  methods: {
+    getOneQuestion() {
+
+      let url = 'https://opentdb.com/api.php?amount=1';
+      if (this.selectedCategory !== 'any') {
+        url = `${url}&category=${this.selectedCategory}`
+      }
+
+      fetch(url)
+        .then(resp => resp.json())
+        .then(data => {
+          this.question = data.results[0]
+          })
+    }
+  },
+  components: {},
+  async created() {
+    const res = await fetch('https://opentdb.com/api_category.php');
+    const data = await res.json()
+    this.categories = [{ id: "any", name: "Any Category"},...data.trivia_categories];
+  }
+};
+</script>
+
+<style src="./assets/styles.css">
---
+++ exercises/25-vue-cli-demo/vue-demo/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="app">
+    <input type="number" v-model="squareWidth" />
+    <br />
+    <input type="range" id="hue" name="hue" min="0" max="255" v-model="hue" />
+    <label for="hue">Hue</label>
+    <br />
+    <div class="square" :class="{}" style="height: 500px" :style="[squareStyles, oneMoreStyle]">
+      <p :class="[hue > 50 ? activeClass : boldClass]">Это какой-то текст</p>
+    </div>
+
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      activeClass: 'is-active',
+      boldClass: 'is-bold',
+      isActive: true,
+      squareWidth: 50,
+      hue: 58,
+      oneMoreStyle: {
+        border: '10px',
+        borderRadius: '20px',
+        height: '300px',
+      },
+    };
+  },
+  computed: {
+    sizeTen() {
+      return this.squareWidth * 10;
+    },
+    squareStyles() {
+      return {
+        width: `${this.sizeTen}px`,
+        height: `${this.sizeTen}px`,
+        backgroundColor: `hsl(${this.hue}, 100%, 50%)`,
+      };
+    },
+  },
+  components: {
+    // HelloWorld,
+  },
+};
+</script>
+
+<style>
+.is-bold {
+  font-weight: bold;
+}
+.is-active {
+  font-size: 2rem;
+}
+.square {
+  width: 100px;
+  height: 100px;
+  padding: 1rem;
+  background-color: hsl(80, 100, 50);
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
