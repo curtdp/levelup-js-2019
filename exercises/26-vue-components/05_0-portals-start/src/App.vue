4d90e2a9baac127539b0c19d9aacfdaf88eea456
Igor Oliinyk <igor.oleynikdp@gmail.com>
2020-04-06 17:11:17 +0300

    Компоненты Vue

---
+++ exercises/26-vue-components/05_0-portals-start/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="min-h-screen p-8 bg-grey-darker">
+      <div class="max-w-sm mx-auto">
+        <user-settings-form @open:confirm-delete-modal="confirmDeleteModalOpen = true"></user-settings-form>
+      </div>
+    </div>
+
+    <confirm-delete-modal
+      :show="confirmDeleteModalOpen"
+      @close="confirmDeleteModalOpen = false"
+      :account-id="accountId"
+    ></confirm-delete-modal>
+  </div>
+</template>
+
+<script>
+import UserSettingsForm from './components/UserSettingsForm.vue';
+import ConfirmDeleteModal from './components/ConfirmDeleteModal.vue';
+
+export default {
+  components: {
+    UserSettingsForm,
+    ConfirmDeleteModal,
+  },
+  data() {
+    return {
+      accountId: 7,
+      confirmDeleteModalOpen: false,
+    };
+  },
+};
+</script>
+
+<style src="./assets/css/app.css" />
---
+++ exercises/26-vue-components/05_0-portals-start/src/components/UserSettingsForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="card">
+    <h1 class="mb-6 text-2xl font-bold">Your Settings</h1>
+    <label class="block mb-6">
+      <span class="block mb-2 font-semibold text-grey-darker">Email Address</span>
+      <input v-model="email" class="form-input" placeholder="you@example.com" />
+    </label>
+    <div class="mb-8">
+      <span class="block mb-2 font-semibold text-grey-darker">Newsletter</span>
+      <div class="flex items-center justify-between">
+        <p class="mr-4">Send you occasional news and updates.</p>
+        <toggle-input v-model="receiveNewsletter" class="flex-no-shrink" />
+      </div>
+    </div>
+    <div class="flex justify-between">
+      <button
+        type="button"
+        class="text-red-dark hover:underline"
+        @click="$emit('open:confirm-delete-modal')"
+      >Delete Account</button>
+      <button type="button" class="btn btn-blue">Update Settings</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import ToggleInput from './ToggleInput.vue';
+
+export default {
+  components: {
+    ToggleInput,
+  },
+  props: ['accountId'],
+  data() {
+    return {
+      email: 'jane@example.com',
+      receiveNewsletter: false,
+    };
+  },
+  methods: {
+    toggleNewsletter() {
+      this.receiveNewsletter = !this.receiveNewsletter;
+    },
+  },
+};
+</script>
