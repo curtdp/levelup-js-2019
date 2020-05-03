c3cde53c89d5ac151cddc088ed1818d0238c260c
Igor Oliinyk <igor.oleynikdp@gmail.com>
2020-05-03 11:42:18 +0300

    Added imageMin

--- exercises/29-movie-db-2020/src/views/About.vue
+++ exercises/29-movie-db-2020/src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <img src="../assets/logo.png" alt="" />
+    <img src="../assets/Tree.png" alt="" />
   </div>
 </template>
--- exercises/29-movie-db-2020/vue.config.js
+++ exercises/29-movie-db-2020/vue.config.js
@@ -1,3 +1,5 @@
+var ImageminPlugin = require('imagemin-webpack-plugin').default;
+
 module.exports = {
   pluginOptions: {
     i18n: {
@@ -6,5 +8,16 @@ module.exports = {
       localeDir: 'locales',
       enableInSFC: true,
     },
+  },
+  configureWebpack: {
+    plugins: [
+      // Make sure that the plugin is after any plugins that add images
+      new ImageminPlugin({
+        disable: process.env.NODE_ENV !== 'production', // Disable during development
+        pngquant: {
+          quality: '95-100',
+        },
+      }),
+    ],
   },
 };
