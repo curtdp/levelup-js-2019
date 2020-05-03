2c74cfc1a68b307814e9443ff9b28b6a6ee46307
Igor Oliinyk <igor.oleynikdp@gmail.com>
2020-05-03 11:50:20 +0300

    config

--- exercises/29-movie-db-2020/vue.config.js
+++ exercises/29-movie-db-2020/vue.config.js
@@ -13,7 +13,7 @@ module.exports = {
     plugins: [
       // Make sure that the plugin is after any plugins that add images
       new ImageminPlugin({
-        disable: process.env.NODE_ENV !== 'production', // Disable during development
+        // disable: process.env.NODE_ENV !== 'production', // Disable during development
         pngquant: {
           quality: '95-100',
         },
