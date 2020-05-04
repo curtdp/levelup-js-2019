52a195af21c69d2e17395154325d9a4d85d68c1b
Igor Oliinyk <igor.oleynikdp@gmail.com>
2020-05-04 15:36:39 +0300

    Lesson 2

--- exercises/31-js-testing/examples/assertion-library.js
+++ exercises/31-js-testing/examples/assertion-library.js
@@ -1,22 +1,33 @@
 const { sum, subtract } = require('../math');
 
-let result, expected;
+test('sum adds numbers', () => {
+  const result = sum(3, 7);
+  const expected = 10;
+  expect(result).toBe(expected);
+});
 
-result = sum(3, 7);
-expected = 10;
+test('subtract subtracts numbers', () => {
+  const result = subtract(7, 3);
+  const expected = 4;
 
-expect(result).toBe(expected);
+  expect(result).toBe(expected);
+});
 
-result = subtract(7, 3);
-expected = 4;
-
-expect(result).toBe(expected);
+function test(title, callback) {
+  try {
+    callback();
+    console.log(`✅ ${title}`);
+  } catch (error) {
+    console.error(`❌ ${title}`);
+    console.error(error);
+  }
+}
 
 function expect(actual) {
   return {
     toBe(expected) {
       if (actual !== expected) {
-        throw new Error(`${result} is not equal to ${expected}`);
+        throw new Error(`${actual} is not equal to ${expected}`);
       }
     },
     toEqual(expected) {},
