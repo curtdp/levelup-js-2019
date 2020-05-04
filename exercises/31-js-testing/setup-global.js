c3feb21c94b804819d44d5f6fc9ee4c0372d532e
Igor Oliinyk <igor.oleynikdp@gmail.com>
2020-05-04 17:59:08 +0300

    Тестирование

---
+++ exercises/31-js-testing/setup-global.js
@@ -0,0 +1,24 @@
+async function test(title, callback) {
+  try {
+    await callback();
+    console.log(`✅ ${title}`);
+  } catch (error) {
+    console.error(`❌ ${title}`);
+    console.error(error);
+  }
+}
+
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`);
+      }
+    },
+    toEqual(expected) {},
+    toBeGreaterThan(expected) {},
+  };
+}
+
+global.test = test;
+global.expect = expect;
