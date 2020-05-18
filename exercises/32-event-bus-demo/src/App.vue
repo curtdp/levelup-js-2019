58fa37f04e8689e99584278345a22742f74b67e6
Igor Oliinyk <igor.oleynikdp@gmail.com>
2020-05-18 18:44:59 +0300

    Vue Event Bus

---
+++ exercises/32-event-bus-demo/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>{{ counter }}</h1>
+    <Increment></Increment>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Increment from './components/Increment.vue'
+import { EventBus } from './event-bus'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Increment
+  },
+  created() {
+    EventBus.$on('increment', () => {
+      this.counter++;
+    })
+  },
+  data() {
+    return {
+      counter: 0,
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ exercises/32-event-bus-demo/src/components/Increment.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button @click="increment">+1</button>
+  </div>
+</template>
+
+<script>
+  import {EventBus} from '../event-bus'
+  export default {
+    methods: {
+      increment() {
+        console.log('increment')
+        EventBus.$emit('increment')
+      }
+    }
+  }
+</script>
